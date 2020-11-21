3b0fb2c4410c99b535c493813e2367c1e79e0794
Samuel Drysén <samuel.drysen@live.se>
2020-11-21 21:04:16 +0100

    moved todolist to index.razor

--- wwwroot/assets/js/script.js
+++ wwwroot/assets/js/script.js
@@ -1,1 +1,34 @@
 ﻿
+
+let foo = 0;
+
+function checkTodo(element) {
+    if(element.checked) {
+        document.querySelector("#foo").classList = "todo-text-crossed";
+    }
+    else {
+        document.querySelector("#foo").classList = "todo-text";
+    }
+}
+
+function newElement() {
+
+    // Get todo text input
+    let inputText = document.querySelector("#new-todo").value;
+
+    // Get todo template
+    let template = document.querySelector("#todo-template").cloneNode();
+
+    // Set text content on template
+    template.textContent = inputText;
+    template.removeAttribute("hidden");
+
+    // add template to todo list
+    document.querySelector("#todo-list-items").append(template);
+
+        //< !--Template -->
+        //    < !--< li id = "todo-template" hidden >
+        //        <input type="checkbox" />
+        //        <p class="todo-text"></p>
+        //    </li > -->
+}

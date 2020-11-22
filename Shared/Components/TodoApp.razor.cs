36893c06019f75d8a19188ab41a58b5b6c422bd5
Samuel Drysén <samuel.drysen@live.se>
2020-11-23 00:05:34 +0100

    scss

---
+++ Shared/Components/TodoApp.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace malformedpeach.github.io.Shared.Components
+{
+    public partial class TodoApp
+    {
+        public string InputText { get; set; }
+        public List<string> TodoItems { get; set; }
+
+
+        public TodoApp()
+        {
+            InputText = "";
+            TodoItems = new List<string>();
+        }
+
+        public void AddTodo()
+        {
+            if(InputText != null && InputText != string.Empty)
+            {
+                TodoItems.Add(InputText);
+            }
+        }
+    }
+}

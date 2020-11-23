49a5c13815d3c16f3b4415414d842575ce8dbc0a
Samuel Drysén <samuel.drysen@live.se>
2020-11-23 23:08:43 +0100

    moar scss

--- Shared/Components/TodoApp.razor.cs
+++ Shared/Components/TodoApp.razor.cs
@@ -9,21 +9,28 @@ namespace malformedpeach.github.io.Shared.Components
     public partial class TodoApp
     {
         public string InputText { get; set; }
-        public List<string> TodoItems { get; set; }
+        public List<Todo> TodoItems { get; set; }
 
 
         public TodoApp()
         {
             InputText = "";
-            TodoItems = new List<string>();
+            TodoItems = new List<Todo>();
         }
 
         public void AddTodo()
         {
             if(InputText != null && InputText != string.Empty)
             {
-                TodoItems.Add(InputText);
+                TodoItems.Add(new Todo { Id = Guid.NewGuid(), TodoText = InputText});
+                InputText = string.Empty;
             }
         }
+    }
+
+    public class Todo
+    {
+        public Guid Id { get; set; }
+        public string TodoText { get; set; }
     }
 }

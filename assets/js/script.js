1f7cd295ce984b83d7f03a143d6656717bcf68e5
malformedpeach <samuel.drysen@live.se>
2022-07-28 11:25:43 +0000

    Deploying to gh-pages from  @ bab052f016c91688551210fc9dbd3c7015f50225 🚀

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+﻿
+function getPreferedTheme() {
+
+    let preferedTheme = "";
+
+    if (window.matchMedia('(prefers-color-scheme: dark').matches) {
+        preferedTheme = "dark";
+    }
+    else if (window.matchMedia('(prefers-color-scheme: light').matches) {
+        preferedTheme = "light";
+    }
+
+    return preferedTheme;
+}
